e18b8c9b6be56e0e4f6452eb87b35b4a70b73f37
MTr4der <93141404+iTr4de@users.noreply.github.com>
2025-02-24 15:28:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from auth_backend import auth_blueprint
+from subscription_backend import subscription_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(auth_blueprint, url_prefix='/api')
+app.register_blueprint(subscription_blueprint, url_prefix='/api')
 
 @app.route('/')
 def home():
@@ -23,6 +25,10 @@ def dashboard():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/subscription')
+def subscription():
+    return render_template('subscription.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
